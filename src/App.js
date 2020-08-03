52ae75f222e00984f63a5de489e8545e0709f102
IamSoPrada <9999goldcoin@gmail.com>
2020-08-03 12:56:53 +0400

    Added pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReactPaginate from "react-paginate";
 import Spinner from "./spinner/spinner";
 import Table from "./table/table";
 import DetailRowView from "./detailrowview/DetailRowView";
 import _ from "lodash";
+import "./App.css"
 
 
 export default class App extends Component {
-    
+
+    _apiBase = "https://api.coinlore.net/api/tickers/";
+
     state = {
         isLoading: true,
         data: [],
         sort: "asc", // desc
         sortField: "id",
-        row: null
+        row: null,
+        currentPage: 0
     }
 
 
     async componentDidMount() {
-
-        const response = await fetch(`https://api.coinlore.net/api/tickers/`);
+        const response = await fetch(`${this._apiBase}`);
         const data = await response.json();
 
         this.setState({
@@ -45,17 +49,45 @@ export default class App extends Component {
             row
         })
     }
-
+    
+    pageChangeHandler = ({selected}) => {
+        this.setState({currentPage: selected})
+        console.log(selected)
+    }
     render() {
+        const pageSize = 50;
+        const displayData = _.chunk(this.state.data, pageSize)[this.state.currentPage]
         return (
             <div className="container d-flex flex-column">
                 {
-                    this.state.isLoading ? <Spinner /> : <Table data={this.state.data}
+                    this.state.isLoading ? <Spinner /> : <Table data={displayData}
                     onSort={this.onSort} 
                     sort = {this.state.sort}
                     sortField = {this.state.sortField}
                     onRowSelect = {this.onRowSelect}/>
 
+                }
+                {
+                this.state.data.length > pageSize ?
+                <ReactPaginate 
+                    previousLabel={'<'}
+                    nextLabel={'>'}
+                    breakLabel={'...'}
+                    breakClassName={'break-me'}
+                    pageCount={10}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.pageChangeHandler}
+                    containerClassName={'pagination'}
+                    activeClassName={'active'}
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    nextClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextLinkClassName="page-link"
+                    forcePage={this.state.currentPage}
+              /> : null
                 }
                 {
                     this.state.row ? <DetailRowView coin={this.state.row}/> : null
--- src/spinner/spinner.js
+++ src/spinner/spinner.js
@@ -2,8 +2,10 @@ import React from 'react'
 import "./spinner.css"
 export default function Spinner() {
     return (
-        <div className="loadingio-spinner-rolling-z1wwj8bd42d"><div className="ldio-qhdjhvthusd">
-        <div></div>
-        </div></div>
+        <div className="wrapper-spinner">
+            <div className="loadingio-spinner-rolling-z1wwj8bd42d"><div className="ldio-qhdjhvthusd">
+            <div></div>
+            </div></div>
+        </div>
     )
 }
